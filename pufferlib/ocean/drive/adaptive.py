1a54cb5137f85a093f708f3b98392f6a09972b5a
Mohit Kulkarni <mkulkarni@ethz.ch>
2026-02-16 07:27:40 -0500

    cleanip

--- pufferlib/ocean/drive/adaptive.py
+++ pufferlib/ocean/drive/adaptive.py
@@ -17,9 +17,6 @@ class AdaptiveDrivingAgent(Drive):
         if human_replay_mode:
             kwargs["co_player_enabled"] = False
 
-        # Remove goal_radius_end - it's used by training loop for curriculum, not Drive
-        kwargs.pop("goal_radius_end", None)
-
         kwargs["resample_frequency"] = self.k_scenarios * self.scenario_length
         self.episode_length = kwargs["resample_frequency"]
         # print(f"resample frequency is ", kwargs["resample_frequency"], flush=True)
